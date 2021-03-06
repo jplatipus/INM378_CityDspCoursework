3247a6e0cfd0d60d29596211b9e8a3f1873e9c1a
jplatipus <jacques@j3ltd.com>
2021-03-06 11:41:05 +0000

    Wav class complete, started on filter convolution

---
+++ pythonCode/FilterClass.py
@@ -0,0 +1,199 @@
+from scipy.io import wavfile
+from scipy import fft, signal
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython.display import display
+
+# Class that applies a filter to a sample
+from pythonCode.WavClass import WavClass
+
+
+class FilterClass:
+    # wav : WavClass
+    # filteredSamples
+
+    def __init__(self, wavClass, doPlot):
+        self.wav = wavClass
+        self.doPlot = doPlot
+
+    # Filter code
+    def __filterAudio__(self, samples):
+        spectrum = fft.rfft(samples)
+        plt.figure()
+        plt.title('Frequency Spectrum of original signal')
+        plt.plot(np.abs(spectrum))
+        plt.show()
+        num_samples = len(samples)
+        freq_response = np.abs(np.cos(2 * np.pi * 0.00005 * np.arange(-num_samples / 2, 1)))
+
+        filtered_freq = spectrum * freq_response
+        if self.doPlot:
+            plt.figure()
+            plt.title("Frequency Filtered Spectrum")
+            plt.plot(np.abs(filtered_freq), 'b', label="Signal Frequencies");
+            plt.xlabel("Frequency (Hz)");
+            plt.ylabel("Amplitude");
+            plt.twinx()  # creates a new y-axis, because the range of y-values of spectrum and freq_response are too
+            # so different that plotting them on the same graph would make freq_response invisible
+            plt.plot(np.abs(freq_response), 'g', label="Filter")
+            plt.xlabel("Frequency");
+            plt.ylabel("Filter Value");
+            plt.legend(loc=4);
+            plt.show()
+        self.filteredSamples = fft.irfft(filtered_freq);
+
+    def __convolveInputSide__(self, signal, kernel):
+        # output of convolution is the length of the signal + the length of the kernel - 1
+        # W. Smith, Digital signal processing, Chapter 6.
+        output = np.zeros(len(signal) + len(kernel) - 1)
+
+        for signalIndex in range(len(signal)):
+            for kernelIndex in range(len(kernel)):
+                output[signalIndex + kernelIndex] = output[signalIndex + kernelIndex] + signal[signalIndex] * kernel[kernelIndex]
+        return output
+
+    def __convolveOutputSide__(selfself, signal, kernel):
+        # output of convolution is the length of the signal + the length of the kernel - 1
+        # W. Smith, Digital signal processing, Chapter 6.
+        output = np.zeros(len(signal) + len(kernel) - 1)
+        output2 = np.ones(len(signal) + len(kernel) - 1)
+        outputRange = range(len(output))
+        kernelRange = range(len(kernel))
+        for outputIndex in outputRange:
+            output2[outputIndex] = 0.0
+            for kernelIndex in kernelRange:
+                if (outputIndex - kernelIndex < 0) or (outputIndex - kernelIndex > (len(signal) - 1)):
+                    # don't do anything
+                    pass
+                else:
+                    output[outputIndex] = output[outputIndex] + kernel[kernelIndex] * signal[outputIndex - kernelIndex]
+                    output2[outputIndex] = output2[outputIndex] + kernel[kernelIndex] * signal[outputIndex - kernelIndex]
+        return output, output2
+
+    def filterSignal(self, samples, filterMin, filterMax, doMine):
+        newSamples = np.zeros(1)
+        if doMine:
+            newSamples = self.__convolveInputSide__(samples, np.concatenate([filterMin, filterMax])) / 2
+        else:
+            newSamples = signal.convolve(samples, np.concatenate([filterMin, filterMax]), mode='same') / 2
+        self.__plotOriginalAndConvolved__(doMine, newSamples, samples)
+        return newSamples
+
+    def __filterAudioStepped__(self, samples, filterMin, filterMax, doMine):
+        windowSteps = int(len(samples) / len(filterMin))
+        stepSize = len(filterMin)
+        newSamples = np.zeros(1)
+        filter = filterMin
+        for step in range(windowSteps):
+            sampleWindowStart = step * stepSize
+            sampleWindow = samples[sampleWindowStart:sampleWindowStart + stepSize]
+            if doMine:
+                conv_samples = self.__convolveInputSide__(sampleWindow, np.concatenate([filter, filter])) / 2
+            else:
+                conv_samples = signal.convolve(sampleWindow, np.concatenate([filter, filter]), mode='same') / 2
+            newSamples = np.concatenate([newSamples, conv_samples])
+            if step % 2 > 0:
+                filter = filterMax
+            else:
+                filter = filterMin
+        self.__plotOriginalAndConvolved__(doMine, newSamples, samples)
+        return newSamples
+
+    def __plotOriginalAndConvolved__(self, doMine, newSamples, samples):
+        plt.figure()
+        plt.title("Original")
+        plt.plot(samples, 'r')
+        plt.plot(newSamples, 'b', alpha=0.5)
+        plt.title("Filtered My Convolution: {}".format(doMine))
+        plt.show()
+
+    '''
+        frequencyResponseSlow = np.abs(np.sin(2*np.pi*0.0002*np.arange(-5000, 1)))
+        plt.plot(frequencyResponseSlow, 'b');
+        slowFilterCoefficient = fft.irfft(frequencyResponseSlow);
+        frequencyResponseFast = np.abs(np.sin(2*np.pi*0.0055*np.arange(-5000, 1)))
+        fastFilterCoefficient = fft.irfft(frequencyResponseFast);
+        plt.plot(np.abs(frequencyResponseFast), 'r');
+        plt.show()
+        #plt.plot(slowFilterCoefficient[5000:195000], 'b');
+        plt.plot(np.abs(slowFilterCoefficient), 'b');
+        plt.show()
+        #plt.plot(slowFilterCoefficient[5000:195000], 'b');
+        plt.plot(np.abs(slowFilterCoefficient), 'b');
+        #plt.show()
+        #plt.plot(fastFilterCoefficient[5000:195000], 'r', alpha=0.5);
+        plt.plot(np.abs(fastFilterCoefficient), 'r', alpha=0.5);
+        plt.show()
+        return slowFilterCoefficient, fastFilterCoefficient
+    '''
+
+    def createCombFilters(self, filterSize, minDelay, maxDelay):
+        # impulse signal for filter1
+        filterMin = np.zeros(filterSize)
+        filterMin[0] = 1
+        filterMin[minDelay] = 0.75
+        # impulse signal for filter 2
+        filterMax = np.zeros(filterSize)
+        filterMax[0] = 1
+        filterMax[maxDelay] = 0.75
+
+        # plot the filters' frequency spectrum
+        minFft = fft.rfft(filterMin)
+        maxFft = fft.rfft(filterMax)
+        if self.doPlot:
+            plt.figure()
+            plt.title("Filter Frequency Spectra")
+            plt.plot(minFft, 'r', alpha=0.5, label='Min Filter')
+            plt.plot(maxFft, 'b', alpha=0.5, label='Max Filter')
+            plt.xlabel("Frequency")
+            plt.ylabel('Amplitude')
+            plt.legend(loc=4)
+            plt.show()
+
+        return filterMin, filterMax
+
+    def filterAudio(self):
+        self.__filterAudio__(self.wav.samplesMono)
+
+    def filterAudioStepped(self, filterMin, filterMax, doManualConvolution):
+        self.__filterAudioStepped__(self.wav.samplesMono, filterMin, filterMax, doManualConvolution)
+
+Test = False
+if Test:
+    # Define wav filename used:
+    s1Filename = "../audio/carrier.wav"
+    s2Filename = "../audio/rockA.wav"
+    s3Filename = "../audio/rockB.wav"
+    display(s1Filename)
+    wavClass = WavClass(s2Filename, False)
+    sig = np.abs(np.sin(2 * np.pi * 0.05 * np.arange(-80, 1)))
+    plt.figure()
+    plt.plot(sig)
+    plt.show()
+    kernel = np.zeros(30)
+    kernel[0] = 1.0
+    kernel[15] = 0.75
+    filterClass = FilterClass(wavClass, True)
+    inputSide = filterClass.__convolveInputSide__(sig, kernel)
+    outputSide, outputSide2 = filterClass.__convolveOutputSide__(sig, kernel)
+    conv = signal.convolve(sig, kernel)
+    plt.figure()
+    plt.plot(inputSide, 'r', label='iSide')
+    plt.plot(outputSide, 'b', alpha = 0.5, label='oSide')
+    plt.plot(conv, 'g', alpha=0.5, label='oSide2')
+    plt.legend()
+    plt.show()
+Test = True
+if Test:
+    # Define wav filename used:
+    s1Filename = "../audio/carrier.wav"
+    s2Filename = "../audio/rockA.wav"
+    s3Filename = "../audio/rockB.wav"
+    display(s1Filename)
+    wavClass = WavClass(s2Filename, False)
+    filterClass = FilterClass(wavClass, True)
+    #filterClass.filterAudio()
+    filterMin, filterMax = filterClass.createCombFilters(64, 8, 32)
+    newSamples = filterClass.filterSignal(wavClass.samplesMono, filterMin, filterMax, True)
+    newSample2 = filterClass.filterSignal(wavClass.samplesMono, filterMin, filterMax, False)
+    #display(Audio(newSamples, rate=s3Rate))
--- pythonCode/WavClass.py
+++ pythonCode/WavClass.py
@@ -4,27 +4,36 @@
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 import numpy as np
-from IPython.display import Audio, display
+from IPython.display import display
 
+#
+# Class that encapsulates the wav file functionality:
+# - load a wav file
+# - plot the wav file's channel(s)
+# - convert to mono using correlation
+# - plot results of the search for maximum correlation if the loaded stereo sample needs correcting
+#
 class WavClass:
-    # samples
-    # sampleRate
-    # filename
-    # isMono
-    # samplesMono
+    # Class properties:
+    # samples the samples as read from the wav file
+    # sampleRate the sample rate
+    # filename the wav file name
+    # isMono boolean if mono samples is 1D, Fals it is 2D
+    # samplesMono the samples converted to mono
 
-    def __init__(self, wavFileName):
-        try:
-            self.sampleRate, self.samples = wavfile.read(wavFileName);
-        finally:
-            display("Error reading wav file ", wavFileName);
+    # constructor loads the given file, converts it to 1 channel mono if it is stereo
+    # plots the sample, and stereo correlation information
+    def __init__(self, wavFileName, doPlots):
+        self.sampleRate, self.samples = wavfile.read(wavFileName);
         self.filename = wavFileName;
+        self.doPlots = doPlots
         if (self.samples.ndim == 2):
             display("Read file: {} {} samples, rate {}".format(self.filename, len(self.samples[:,0]), self.sampleRate))
             self.__convertToMono__()
         else:
             display("Read file: {} {} samples, rate {}".format(self.filename, len(self.samples[:]), self.sampleRate))
             self.samplesMono = self.samples
+            self.__plotMono__("One Channel Sample File Plot")
 
 
     # Plot stereo samples, and mono samples on the same graph to see the result of
@@ -33,6 +42,9 @@ class WavClass:
     #       monoSamples: 1d array of samples
     #       title: title to give to the plot
     def __plotStereo__(self, title):
+        if not self.doPlots:
+            return
+
         fig, ax1 = plt.subplots()
         ax1.set_title(title)
         ax1.plot(self.samples[:,0], label="Ch. 1", alpha=0.35, color='green')
@@ -44,9 +56,10 @@ class WavClass:
         plt.show()
 
     # Plot monophonic (mono) samples
-    #       monoSamples: 1d array of samples
     #       title: title to give to the plot
     def __plotMono__(self, title):
+        if not self.doPlots:
+            return
         fig, ax1 = plt.subplots()
         ax1.set_title(title)
         ax1.plot(self.samplesMono, 'b')
@@ -57,9 +70,6 @@ class WavClass:
     # Using Audacity, on RockA.wav, it can be seen that the stereo channels are not in phase,
     # and one channel has been inverted. This function finds how many samples to shift and invert
     # the respective channels
-    # Input: stereo 2d list of channel1 and channel2 samples
-    #        sampleRate: sampling rate
-    # Output: unchanged stereo if no good correlation found, corrected samples otherwise.
     def __alignSamples__(self):
         correlationCoefficients = np.zeros(self.sampleRate)
         channel1Section = self.samples[0:self.sampleRate,0] * -1
@@ -68,8 +78,10 @@ class WavClass:
         maxCoefficient = 0.0
         maxCoefficientIndex = 0
 
+        # loop shifting channel 1 to the right to find the highest correlation coefficient between channel 1 & 2
         for offset in range(0,int(self.sampleRate/10)):
             #rawCoeff = np.dot(self.samples[:, 0], self.samples[:, 1]) / np.sqrt(np.dot(self.samples[:, 0], self.samples[:, 0]) * np.dot(self.samples[:, 1], self.samples[:, 1]))
+            # calculate the correlation coefficient of the two channels at the currently shifted position
             correlationCoefficients[offset] = np.dot(channel1RightShift, channel2Section) / np.sqrt(np.dot(channel1RightShift, channel1RightShift) * np.dot(channel2Section, channel2Section))
             channel1RightShift = np.concatenate([np.zeros(1), channel1RightShift])
             channel2Section = np.concatenate([channel2Section, np.zeros(1)])
@@ -88,7 +100,12 @@ class WavClass:
             # new in phase stereo is constructed
             self.samples = np.transpose(np.array([channel1, channel2]))
 
+    # plot the results of the search for the maximum correlation coefficient
+    # Input: correlationCoefficients 1d array of correlation coefficients
+    #        maxCoefficientIndex index in the array of the correlation coefficient that is the largest
     def __plotChannelAlignmentCorrelationCoefficientSearch__(self, correlationCoefficients, maxCoefficientIndex):
+        if not self.doPlots:
+            return
         # plot all correlation coefficients calculated
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
@@ -96,18 +113,18 @@ class WavClass:
         ax.set_title("correlationCoefficients")
         ax.set_xlabel("Channel 1 right shift value")
         ax.set_ylabel("Channel 1&2 correlation coefficient")
+
         # set xaxis labels
         ticks = np.array(range(maxCoefficientIndex + 10))
         labels = ticks.astype(str)
         ax.set_xticks(ticks)
         ax.set_xticklabels(labels)
+        plt.grid()
         plt.show()
         print("Max coefficient: ", correlationCoefficients[0:maxCoefficientIndex])
 
     # Convert the stereo sample to a mono sample, displays a plot of samples and conversion
-    # input: sampled2D a 2d array of sample values
-    #        sampleRate the smpling rate of the samples
-    # returns a 1d array of the samples converted to mono
+    #
     def __convertToMono__(self):
         display("Converted to mono: {}".format(self.filename))
         # calculate correlation coefficient
@@ -116,7 +133,7 @@ class WavClass:
         if rawCoeff >= 0.8:
             # no need to correct the samples
             # plot them:
-            self.__plotStereo__(self.samples, self.samplesMono, "Good conversion to mono (correlation coefficient >= 0.8:) {}".format(rawCoeff));
+            self.__plotStereo__("Good conversion to mono (correlation coefficient >= 0.8:) {}".format(rawCoeff));
         else:
             # samples need correcting, plot the original:
             self.__plotStereo__("Poor conversion to mono (correlation coefficient < 0.8): {}".format(rawCoeff))
@@ -126,8 +143,8 @@ class WavClass:
             self.samplesMono = self.samples[:, 0]/2 + self.samples[:, 1]/2;
             # plot the corrected samples
             self.__plotStereo__("Aligned conversion to mono (correlation coefficient < 0.8): {}".format(rawCoeff))
-
-# Set to True to test this cell
+        self.__plotMono__("Resulting Mono Sample")
+# Set to True to test this class
 TEST = False
 if TEST:
     # Define wav filename used:
@@ -136,4 +153,5 @@ if TEST:
     s3Filename = "../audio/rockB.wav"
     display(s2Filename)
     wavClass = WavClass(s2Filename)
+    # code below only works in a notebook:
     #display(Audio(wavClass.samples, rate=wavClass.sampleRate, normalize=False));
